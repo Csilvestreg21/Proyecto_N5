55d0c8ec498b3e58e7a9225b19149978591eff1c
CatalinaS <kattha16@gmail.com>
2023-12-22 13:20:01 -0500

    feat: Agregar funcionalidad selección de vuelo origen y destino

--- cypress/Pages/selectTravelPage.js
+++ cypress/Pages/selectTravelPage.js
@@ -1,4 +1,6 @@
+// Clase y métodos estáticos para la selección de viaje
 class SelectTravel {
+    // Método estático para seleccionar la moneda y país
     static selectMoney() {
         cy.get('.sc-ptRml > .sc-plVjM > #currency-wrapper > #header__currentCurrency > .sc-fzoXWK').click()
         cy.get('.sc-pZOBi > #country-lang-selector-select-country--select > #country-lang-selector-select-country--select__trigger > #country-lang-selector-select-country--select__trigger--text-field__wrapper > #country-lang-selector-select-country--select__trigger--text-field').click()
@@ -7,100 +9,46 @@ class SelectTravel {
         cy.get('#dialog-country-lang-selector > #dialog-country-lang-selector--dialog__body > .sc-fzonjX > .sc-pBzUF > #country-lang-selector-continue-button').click()
         cy.visit('https://www.latamairlines.com/us/es')
         cy.get('#country-suggestion > #country-suggestion--dialog__body > .sc-fzonjX > .sc-pLwIe > #country-suggestion-reject-change').click()
-      }
+    }
 
-      static selectOrigin(origin) {
+    // Método estático para seleccionar el origen del viaje
+    static selectOrigin(origin) {
+        cy.get('#optionSection > .sc-qQjSQ > #btnAddPassengerCTA > .sc-pJvck > span:nth-child(1)').click()
+        cy.get('#sectionAddChildren > .sc-oTBjG > .sc-pJUfC > #btnPlusChildren > .sc-prsLc').click()
         cy.get('.sc-qbDfu > .sc-pKJXg > .sc-pCNjN > #txtInputOrigin > #txtInputOrigin_field').click()
         cy.get('.sc-qbDfu > .sc-pKJXg > .sc-pCNjN > #txtInputOrigin > #txtInputOrigin_field').type('Mede')
         cy.get('#listAutoComplete > #lstItem_0 > #btnItemAutoComplete_0 > .sc-pQSDG > .sc-pIwMV:nth-child(3)').click()
-      }
 
-      static clickContinueButton() {
+    }
+
+    // Método estático para hacer clic en el botón de continuar
+    static clickContinueButton() {
+        // Prueba para verificar el flujo de clic en el botón de continuar
         cy.get('#btnContinueCTA').click();
-      }
-      static selectDestination(destination){
+    }
+
+    // Método estático para seleccionar el destino del viaje
+    static selectDestination(destination) {
+        // Prueba para verificar el flujo de selección del destino del viaje
         cy.get('.sc-qbDfu > .sc-pKJXg > .sc-pCNjN > #txtInputDestination > #txtInputDestination_field').type('Bog')
         cy.get('#listAutoComplete > #lstItem_0 > #btnItemAutoComplete_0 > .sc-pQSDG > .sc-pIwMV:nth-child(5)').click()
         cy.get('.sc-pKJXg > .sc-pCNjN > .sc-pkTlu > .sc-psPnT > #departureDate').click()
-        cy.get('.CalendarMonthGrid_month__horizontal:nth-child(3) > .CalendarMonth > .CalendarMonth_table > tbody > tr:nth-child(3) > .CalendarDay:nth-child(3)').click()
-        cy.get('.CalendarMonthGrid_month__horizontal:nth-child(3) > .CalendarMonth > .CalendarMonth_table > tbody > tr:nth-child(3) > .CalendarDay:nth-child(7)').click()
+        cy.get('.CalendarMonthGrid_month__horizontal:nth-child(3) > .CalendarMonth > .CalendarMonth_table > tbody > tr:nth-child(3) > .CalendarDay:nth-child(2)').click()
+        cy.get('.CalendarMonthGrid_month__horizontal:nth-child(3) > .CalendarMonth > .CalendarMonth_table > tbody > tr:nth-child(3) > .CalendarDay:nth-child(5)').click()
         cy.get('#inputSection > .sc-qbDfu > .sc-pKJXg > .sc-pCNjN > #btnSearchCTA').click()
-        cy.visit('https://www.latamairlines.com/co/es/ofertas-vuelos?origin=MDE&inbound=2024-01-04T17%3A00%3A00.000Z&outbound=2023-12-28T17%3A00%3A00.000Z&destination=BOG&adt=1&chd=1&inf=0&trip=RT&cabin=Economy&redemption=false&sort=RECOMMENDED')
-        .wait(5000);
-      }
-      static selectSearchFlight(){
-        cy.get('[data-test="dcad621dd5"] > .cardFlightstyle__WrapperCardShadow-sc__sc-1fa5kbc-0 > .cardFlightstyle__CardExpander-sc__sc-1fa5kbc-2').click();
+        cy.visit('https://www.latamairlines.com/co/es/ofertas-vuelos?origin=MDE&inbound=2024-01-19T17%3A00%3A00.000Z&outbound=2024-01-16T17%3A00%3A00.000Z&destination=BOG&adt=1&chd=1&inf=0&trip=RT&cabin=Economy&redemption=false&sort=RECOMMENDED')
+         .wait(5000);
+    }
+
+    // Método estático para buscar vuelos
+    static selectSearchFlight() {
+        cy.get('[data-test="5805dd2a90"] > .cardFlightstyle__WrapperCardShadow-sc__sc-1fa5kbc-0 > .cardFlightstyle__CardExpander-sc__sc-1fa5kbc-2').click();
         cy.get("[data-testid='bundle-detail-3-flight-select']").click();
         cy.wait(3000);
-        cy.get('[data-test="e990999847"] > .cardFlightstyle__WrapperCardShadow-sc__sc-1fa5kbc-0').click();
+        cy.get('[data-test="12a66d2eba"] > .cardFlightstyle__WrapperCardShadow-sc__sc-1fa5kbc-0 > .cardFlightstyle__CardExpander-sc__sc-1fa5kbc-2').click();
         cy.get('[data-testid="bundle-detail-3-flight-select"]').click();
         cy.wait(10000);
-      }
+    }
+}
 
-      static selectSeatOrigin(){
-        cy.get('[data-testid="button9--button"]').click();
-        cy.visit('https://www.latamairlines.com/co/es/seleccion-asientos?id=LA0351546FYHG')
-        cy.wait(4000);
-        cy.get(':nth-child(3) > :nth-child(3) > .MapSlot__SeatContent-sc-18zijkx-4 > .Seat__ButtonSeat-sc-1xsdv1h-0').click()
-        cy.wait(4000);
-        cy.get(':nth-child(3) > :nth-child(1) > .MapSlot__SeatContent-sc-18zijkx-4 > .Seat__ButtonSeat-sc-1xsdv1h-0').click()
-        cy.wait(4000);
-        cy.get('[data-testid="btn-next-flight-button"]').click()
-      }
-      static selectSeatDestination(){
-        cy.wait(4000);
-        cy.get(':nth-child(2) > :nth-child(2) > .MapSlot__SeatContent-sc-18zijkx-4 > .Seat__ButtonSeat-sc-1xsdv1h-0').click()
-        cy.wait(4000);
-        cy.get(':nth-child(3) > :nth-child(3) > .MapSlot__SeatContent-sc-18zijkx-4 > .Seat__ButtonSeat-sc-1xsdv1h-0').click()
-        cy.wait(4000);
-        cy.get('[data-testid="btn-confirm-and-continue-button"]').click()
-      }
-
-      static selectBaggage(){
-        cy.wait(4000);
-        cy.visit('https://www.latamairlines.com/co/es/tienda?id=LA0351546FYHG')
-      }
-
-      static selectEspecialBaggage(){
-        cy.wait(4000);
-        cy.get('.ListBaggages__ListSectionBaggage-eaweis-0 > .SectionBaggage__SectionBaggageWrapper-rccyl7-4 > .SectionBaggage__ListOffersWrapper-rccyl7-2 > .SpecialBaggage__ListContentSpecial-sc-6nr771-4 > #accordion-special-baggages').click()
-        cy.wait(6000);
-        cy.visit('https://www.latamairlines.com/co/es/centro-ayuda/preguntas/equipaje/especial');
-        cy.get('[data-testid="main-content-sidebar-equipaje-1-level-1-item-equipaje-especial-4-level-2-item-sidebarListItem"]').click()
-        cy.wait(2000);
-        cy.visit('https://www.latamairlines.com/co/es/tienda?id=LA0351546FYHG');
-      }
-
-      static clickContinueButtonP()
-      {
-        cy.get('#BAGS-content > .MuiAccordionDetails-root > .sc-jXbUNg > #BAGS-continue-button > .MuiButton-label').click()
-        cy.get('.Aside__AsideStyle-m8xp39-0 > .Aside__AsideFooter-m8xp39-2 > .CartSection__ContentCart-ylshii-0 > .sc-jXbUNg > #button-cart-confirm').click()
-        cy.visit('https://www.latamairlines.com/co/es/pagos/pasajeros?orderId=LA0351546FYHG')
-      }
-
-       static passengerG()
-      {
-        cy.get('[data-testid="passengerDetails-firstName-ADT_1"] > .MuiInputBase-root').type('Testing')
-        cy.get('[data-testid="passengerDetails-lastName-ADT_1"] > .MuiInputBase-root').type('Tester')
-        cy.get('#passengerInfo-dateOfBirth-ADT_1').click()
-        cy.get('[data-testid="documentInfo-documentNumber-ADT_1"] > .MuiInputBase-root').type('1092312312')
-        cy.get('[data-testid="passengerInfo-emails-ADT_1"] > .MuiInputBase-root').click()
-        cy.get('[data-testid="passengerInfo-phones0-number-ADT_1"] > .MuiInputBase-root').type('1234567809')
-        cy.get('[data-testid="passengerFormSubmitButtonADT_1"]').click()
-         cy.wait(2000);
-      }
-
-       static passengerJ()
-      {
-        cy.get('[data-testid="accordion-passenger-CHD_1-accordion"]')
-        cy.get('[data-testid="passengerDetails-firstName-CHD_1"] > .MuiInputBase-root').type('tester')
-        cy.get('[data-testid="passengerDetails-lastName-CHD_1"] > .MuiInputBase-root').type('jr')
-        cy.get('#passengerInfo-dateOfBirth-CHD_1').type('12/12/2022')
-        cy.get('[data-testid="documentInfo-documentNumber-CHD_1"] > .MuiInputBase-root').type('1231231233')
-        cy.get('[data-testid="passengerInfo-emails-CHD_1"] > .MuiInputBase-root')
-        cy.get('[data-testid="passengerInfo-phones0-number-CHD_1"] > .MuiInputBase-root')
-        cy.get('[data-testid="passengerFormSubmitButtonCHD_1"]').click()
-        cy.wait(2000);
-        cy.get('[data-testid="undefined-button"]').click()
-      }
- }
+export default SelectTravel;
