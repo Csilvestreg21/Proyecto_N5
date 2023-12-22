8ad51504fc94b5e3dc7c93c9d56aeb99455b2c4e
CatalinaS <kattha16@gmail.com>
2023-12-22 13:14:01 -0500

    Ajuste de POM

--- cypress/e2e/HomeLatam.cy.js
+++ cypress/e2e/HomeLatam.cy.js
@@ -1,23 +1,30 @@
+// Importa las páginas relacionadas con el flujo de reserva
 import HomePage from '../pages/homePage.js';
 import SelectTravelPage from '../pages/selectTravelPage.js';
+import SelectSeatPage from '../pages/selectSeatPage.js';
+import SelectPassengerPage from '../pages/selectPassengerPage.js';
+import SelectPaymentPage from '../pages/selectPaymentPage.js';
 
+// Descripción: Define una suite de pruebas para el manejo de la plantilla.
 describe('Template Spec', () => {
-  before(() => {
-    HomePage.visitHomePage();
-  });
+    // Antes de ejecutar las pruebas, visita la página de inicio.
+    before(() => {
+        HomePage.visitHomePage();
+    });
 
-it.only('Test Tesla Link', () => {
-    // Paso a paso, utilizando métodos del Page Object
-    HomePage.selectMoney();
-    HomePage.selectOrigin('bog');
-    HomePage.selectDestination('Med')
-    HomePage.selectSearchFlight()
-    HomePage.selectSeatOrigin()
-    HomePage.selectSeatDestination()
-    HomePage.selectBaggage()
-    HomePage.selectEspecialBaggage()
-    HomePage.clickContinueButtonP()
-    HomePage.passengerG()
-    HomePage.passengerJ()
-  });
+    it.only('Test Latam', () => {
+        // Prueba para verificar el flujo completo de reserva de viaje en Latam
+        SelectTravelPage.selectMoney();
+        SelectTravelPage.selectOrigin('bog');
+        SelectTravelPage.selectDestination('Med');
+        SelectTravelPage.selectSearchFlight();
+        SelectSeatPage.selectSeatOrigin();
+        SelectSeatPage.selectSeatDestination();
+        SelectSeatPage.selectBaggage();
+        SelectSeatPage.selectEspecialBaggage();
+        SelectSeatPage.clickContinueButtonP();
+        SelectPassengerPage.passengerG();
+        SelectPassengerPage.passengerJ();
+        SelectPaymentPage.methodPayment();
+    });
 });
